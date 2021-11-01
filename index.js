7a17b5bef0705cdc18c28a61e2c445247a08c7c0
sujanshaikh369 <sush11112011@gmail.com>
2021-11-01 15:33:49 +0600

    Commited

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = 5000;
 
-// middleware
+// middleware code
 app.use(cors());
 app.use(express.json());
 // user: mydbuser1
@@ -22,6 +22,7 @@ async function run() {
     await client.connect();
     const database = client.db("foodMaster");
     const deliveryCollection = database.collection("deliver");
+    const orderCollection = database.collection("order");
 
     //GET THE API
     app.get("/deliver", async (req, res) => {
@@ -30,7 +31,7 @@ async function run() {
       // console.log(result);
     });
 
-    //CREATE A DYNMICE API
+    //CREATE A DYNAMIC API
     app.get("/deliver/:id", async (req, res) => {
       const id = req.params.id;
       console.log("getting specific service", id);
@@ -39,7 +40,46 @@ async function run() {
       res.json(service);
       console.log(service);
     });
-    
+    // CREATE A ORDER API
+    app.post("/order", async (req, res) => {
+      const data = req.body;
+      const result = await orderCollection.insertOne(data);
+      res.send(result);
+    });
+    //GET THE ORDER API
+    app.get("/order", async (req, res) => {
+      const result = await orderCollection.find({}).toArray();
+      res.send(result);
+      // console.log(result);
+    });
+
+    //DELETE THE ORDER API
+    app.delete("/order/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await orderCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+      console.log(result);
+    });
+    //my order get api with email
+    app.get("/Order/:email", async (req, res) => {
+      const result = await orderCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    //DELETE THE ORDER API
+    app.delete("/Order/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await orderCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
   } finally {
     //   await client.close();
   }
